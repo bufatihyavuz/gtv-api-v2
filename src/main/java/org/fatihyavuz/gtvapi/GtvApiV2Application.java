128f2baa8ba77f4536ea072c388073befc417a51
bufatihyavuz <bu.fatih.yavuz@gmail.com>
2025-04-28 20:11:48 +0300

    first commit

---
+++ src/main/java/org/fatihyavuz/gtvapi/GtvApiV2Application.java
@@ -0,0 +1,13 @@
+package org.fatihyavuz.gtvapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtvApiV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtvApiV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/fatihyavuz/gtvapi/GtvApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fatihyavuz.gtvapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtvApiV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
