5acb09e8d1173ba234c4d4aa2477b35c5f6bd94b
bufatihyavuz <bu.fatih.yavuz@gmail.com>
2025-06-03 22:45:56 +0300

    /register servisi auth altına taşındı

--- src/main/java/org/gtvapi/controller/AuthController.java
+++ src/main/java/org/gtvapi/controller/AuthController.java
@@ -1,31 +1,35 @@
 package org.gtvapi.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.gtvapi.EntityStatus;
+import org.gtvapi.entity.User;
+import org.gtvapi.login.jwt.model.AuthRequest;
 import org.gtvapi.login.jwt.model.AuthResponse;
 import org.gtvapi.login.jwt.model.LoginRequest;
+import org.gtvapi.repository.UserRepository;
 import org.gtvapi.service.AuthService;
-import org.gtvapi.util.DirectoryUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
 
     private static final Logger logger = LogManager.getLogger(AuthController.class);
 
     private final AuthService authService;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
@@ -42,6 +46,25 @@ public class AuthController {
         // (isteğe bağlı) token blackliste alınabilir
         logger.info("logout yapıldı");
         return ResponseEntity.ok().body("Logged out");
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody AuthRequest request) {
+        if (userRepository.existsByUsername(request.getUsername())) {
+            return ResponseEntity.badRequest().body("Kullanıcı adı zaten var.");
+        }
+
+        User newUser = new User();
+        newUser.setUsername(request.getUsername());
+        newUser.setPassword(passwordEncoder.encode(request.getPassword()));
+        newUser.setName(request.getName());
+        newUser.setSurname(request.getSurname());
+        newUser.setEmail(request.getEmail());
+        newUser.setRecordStatus(EntityStatus.ACTIVE.getCode());
+
+        userRepository.save(newUser);
+
+        return ResponseEntity.ok("Kayıt başarılı");
     }
 
 
